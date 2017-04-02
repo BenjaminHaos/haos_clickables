e8b434bf007bbb17c882caca5d5c1e91845e7739
KoreaHaos <koreahaos@gmail.com>
2017-04-02 01:09:52 +0000

    Have clickable gist page create proper titles for gists.

--- js/haos_clickable_gists.js
+++ js/haos_clickable_gists.js
@@ -29,7 +29,11 @@ function create_array_of_clickable_gist_names_and_raw_url(user_dom_should_work_w
                     var clickable_gist_description = default_user_object[i]["description"];
                     var clickable_gist_github_url = default_user_object[i]["html_url"];
                     
-                    var clickable_gist_html_link = '<a class="clickable_gist_link" href="' + clickable_gist_github_url + '" target="_blank">' + clickable_gist_filename + '</a>';
+                    var clickable_gist_title = clickable_gist_filename.substring(0, clickable_gist_filename.indexOf('_CLICKABLE'));
+                    clickable_gist_title=clickable_gist_title.replace(/_/g, ' ');
+                    clickable_gist_title=make_string_a_title(clickable_gist_title);
+                    var clickable_gist_html_link = '<a class="clickable_gist_link" href="' + clickable_gist_github_url + '" target="_blank">' + clickable_gist_title + '</a>';
+                    
                     var id_for_clickable = clickable_gist_filename.substring(0, clickable_gist_filename.indexOf('.')) + "_clickable"
 
 
@@ -56,6 +60,17 @@ function requestJSON(url, callback) {
             callback.call(null, xhr.responseJSON);
         }
     });
+}
+
+function make_string_a_title(str) {
+     str = str.toLowerCase().split(' ');                // will split the string delimited by space into an array of words
+
+     for(var i = 0; i < str.length; i++){               // str.length holds the number of occurrences of the array...
+          str[i] = str[i].split('');                    // splits the array occurrence into an array of letters
+          str[i][0] = str[i][0].toUpperCase();          // converts the first occurrence of the array to uppercase
+          str[i] = str[i].join('');                     // converts the array of letters back into a word.
+     }
+     return str.join(' ');                              //  converts the array of words back to a sentence.
 }
 
 init_clickable_gists();
