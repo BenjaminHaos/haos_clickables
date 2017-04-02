191f628c8b7be9ef3ebbd00cefb01648d6b6502f
KoreaHaos <koreahaos@gmail.com>
2017-04-02 01:24:53 +0000

    Clean up code.

--- js/haos_clickable_gists.js
+++ js/haos_clickable_gists.js
@@ -1,5 +1,5 @@
+/* global $*/
 var default_username = "BenjaminHaos";
-var default_user_object;
 
 function init_clickable_gists() {
     create_array_of_clickable_gist_names_and_raw_url();
@@ -10,38 +10,30 @@ function create_array_of_clickable_gist_names_and_raw_url(user_dom_should_work_w
     var url_to_call = basic_github_user_api_string + default_username + "/gists";
 
     requestJSON(url_to_call, function(json) {
-        var basic_user_data_html_string = "NOT_SET";
-        var error_string = "GitHub API Rate Limit Exceeded!"
-
-        if (json.message == "Not Found") {
-            basic_user_data_html_string = error_string;
-        }
-        else {
-            default_user_object = json;
-        }
-        for (var i = 0; i < default_user_object.length; i++) {
-            gist_file_name_array = Object.keys(default_user_object[i].files);
-            for (k = 0; k < Object.keys(default_user_object[i].files).length; k++) {
+        for (var i = 0; i < json.length; i++) {
+            var gist_file_name_array = Object.keys(json[i].files);
+            for (var k = 0; k < Object.keys(json[i].files).length; k++) {
                 if (gist_file_name_array[k].includes("_CLICKABLE")) {
                     var clickable_gist_filename = gist_file_name_array[k];
-                    var clickable_gist_raw_url = default_user_object[i].files[gist_file_name_array[k]].raw_url;
+                    var clickable_gist_raw_url = json[i].files[gist_file_name_array[k]].raw_url;
 
-                    var clickable_gist_description = default_user_object[i]["description"];
-                    var clickable_gist_github_url = default_user_object[i]["html_url"];
+                    var clickable_gist_description = json[i]["description"];
+                    var clickable_gist_github_url = json[i]["html_url"];
                     
                     var clickable_gist_title = clickable_gist_filename.substring(0, clickable_gist_filename.indexOf('_CLICKABLE'));
                     clickable_gist_title=clickable_gist_title.replace(/_/g, ' ');
                     clickable_gist_title=make_string_a_title(clickable_gist_title);
+                    
                     var clickable_gist_html_link = '<a class="clickable_gist_link" href="' + clickable_gist_github_url + '" target="_blank">' + clickable_gist_title + '</a>';
                     
-                    var id_for_clickable = clickable_gist_filename.substring(0, clickable_gist_filename.indexOf('.')) + "_clickable"
+                    var id_for_clickable = clickable_gist_filename.substring(0, clickable_gist_filename.indexOf('.')) + "_clickable";
 
 
                     var html_line_for_label = "<label for=\"" + id_for_clickable + "\">" + clickable_gist_html_link + "</label>";
 
                     var text_for_clickable = "bash <(curl -s " + clickable_gist_raw_url + ")";
                     var html_line_clickable = '<input type="text" id="' + id_for_clickable + '" value="' + text_for_clickable + '" />';
-                    var html_line_for_clickable_button = '<button title="' + clickable_gist_description + '" data-copytarget="#' + id_for_clickable + '">copy</button>'
+                    var html_line_for_clickable_button = '<button title="' + clickable_gist_description + '" data-copytarget="#' + id_for_clickable + '">copy</button>';
 
                     $("#clickable_gists").append(html_line_for_label);
                     $("#clickable_gists").append(html_line_clickable);
@@ -50,7 +42,6 @@ function create_array_of_clickable_gist_names_and_raw_url(user_dom_should_work_w
             }
         }
     });
-
 }
 
 function requestJSON(url, callback) {
@@ -62,6 +53,7 @@ function requestJSON(url, callback) {
     });
 }
 
+// Sauce : https://www.sitepoint.com/community/t/capitalizing-first-letter-of-each-word-in-string/209644
 function make_string_a_title(str) {
      str = str.toLowerCase().split(' ');                // will split the string delimited by space into an array of words
 
